aa12971aeaabf206e31d5393e7d963978d3ac0e8
leonardobarral <rm551472@fiap.com.br>
2023-08-24 22:49:07 -0300

    Primeiros Passo no React Fiap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import reactLogo from "./assets/react.svg"
+import Cabecalho from "./components/Cabecalho"
 
 export default function App() {
   // Área declarativa
-
+  let reactLogoTextoAlt = "Logo React"
 
   return (
     <>
       {/* Área imperativa */}
       <div>
-        <h1>Vite + React</h1>
-        <h2>Conseguimos</h2>
-        <p>Olá Mundo!!!</p>
+        {/* um cabeçalho Header, um h1, uma lista ul com 3 itens com 3 links */}
+        <Cabecalho/>
+        {/* uma seção com uma div com 3 paragrafos, com 3 linhas de lorem uma imagem */}
+        <section>
+          <p>uma seção com uma div com 3 paragrafos, com 3 linhas de lorem uma imagem</p>
+          <p>uma seção com uma div com 3 paragrafos, com 3 linhas de lorem uma imagem</p>
+          <p>uma seção com uma div com 3 paragrafos, com 3 linhas de lorem uma imagem</p>
+          <img src={reactLogo} alt= {reactLogoTextoAlt} />
+        </section>
+        {/* um rodape com uma div, uma lista ul com 3 item e links para redes sociais */}
+        <footer>
+          <div>
+            <ul>
+              <li><a href="GitHub"></a></li>
+              <li><a href="X"></a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </div>
+        </footer>
       </div>
     </>
   )
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,19 @@
+export default function Cabecalho(){
+
+    return(
+        <>
+            
+            <header>
+                <h1></h1>
+                <ul>
+                <li><a href="#">Item - 1</a></li>
+                <li><a href="#">Item - 1</a></li>
+                <li><a href="#">Item - 1</a></li>
+                
+                </ul>
+            </header>
+
+        </>
+        
+    )
+}
